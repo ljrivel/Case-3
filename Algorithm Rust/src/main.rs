cf5437710463d6c517c374f533b33b28f790cdaa
ljrivel <ljrivel@gmail.com>
2021-03-05 11:31:52 -0600

    Rust Algoritm ready

---
+++ Algorithm Rust/src/main.rs
@@ -0,0 +1,21 @@
+extern crate rand;
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let mut _list = [4; 500];
+     
+    for _i in 0.._list.len(){
+        _list[_i] = rng.gen_range(0..11);
+    }
+    
+    
+    for _i in 0.._list.len(){
+        if _list[_i] == -1 {
+            println!("Encontre el numero: -1 ");
+        }
+    }
+    
+     println!(" ");
+    println!("Termino de correr el programa ");
+}
