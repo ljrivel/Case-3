4d0baa1cc18cee6e9b8f50ed202f136094a7328b
Fabian Granados Siles <80019288+Fabioncho0514@users.noreply.github.com>
2021-03-05 22:41:05 -0600

    Algoritmos finalizados
    Adjunto los algoritmos de c y c++ listos

--- Algorithm C/c.c
+++ Algorithm C/c.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 int main(){
+    unsigned t0;
+    t0=clock();
     int arreglo[500000];
     for(int i=0; i<500000; i++){
         arreglo[i]=rand()%100;
@@ -10,4 +14,6 @@ int main(){
             return 0;
         }
     }
+    double tiempo=1000L*(clock()-t0)/CLOCKS_PER_SEC;
+    printf("Tiempo de ejecucion es %f ms", tiempo);
 }
