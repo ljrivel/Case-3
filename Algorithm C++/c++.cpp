a9e8df4828fc5e05a1f4d8d75d052fa79e869f4d
Fabian Granados Siles <80019288+Fabioncho0514@users.noreply.github.com>
2021-03-08 20:24:08 -0600

    Final 8/03/2021

--- Algorithm C++/c++.cpp
+++ Algorithm C++/c++.cpp
@@ -1,12 +1,18 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#include <windows.h>
 
 using namespace std;
 
 int main(){
-    unsigned tiempoInicial;
+    double tiempoInicial;
     tiempoInicial=clock();
+    MEMORYSTATUSEX status;
+    double memoriaUtilizada;
+    status.dwLength=sizeof(status);
+    GlobalMemoryStatusEx(&status);
+    memoriaUtilizada=status.ullAvailPhys/1024;
     int arreglo[500000];
     for(int i=0; i<500000; i++){
         arreglo[i]=rand()%100;
@@ -17,6 +23,10 @@ int main(){
         }
     }
     double tiempo=1000L*(clock()-tiempoInicial)/CLOCKS_PER_SEC;
+
     cout<<"Tiempo de ejecucion es "<<tiempo<<" ms"<<endl;
-        
+    status.dwLength=sizeof(status);
+    GlobalMemoryStatusEx(&status);
+    memoriaUtilizada-=(status.ullAvailPhys/1024);
+    cout<<"Memoria utilizada por el programa "<<memoriaUtilizada<<" kb"<<endl;
 }
